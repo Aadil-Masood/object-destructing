c86dca830c5e25b5b4b3c3a821e29f60447170a0
Aadil-Masood <aadilsiddiqui1995@gmail.com>
2021-08-28 10:18:56 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+
+//object destructing
+const fruits =['orange','banana','lemon']
+const [fruits,second,third]=fruits;
+console.log(first)
+
+const number =[1,2,3,4]
+const numberTwo=[6,7,8,9]
+
+const combine =[...number,...numberTwo]
+console.log(combine)
+
+const first ={name:'Aadil'}
+const second ={job:'development'}
+const combine={...first,place:'Delhi',...second}
+console.log(combine)
